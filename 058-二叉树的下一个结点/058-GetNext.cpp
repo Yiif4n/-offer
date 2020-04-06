70eca51552c913c9e2e6b7ddde4c0bb31a7573f1
Yiif4n <45288677+Yiif4n@users.noreply.github.com>
2020-04-07 00:05:50 +0800

    Create 058-GetNext.cpp

---
+++ 058-二叉树的下一个结点/058-GetNext.cpp
@@ -0,0 +1,53 @@
+/*
+struct TreeLinkNode {
+    int val;
+    struct TreeLinkNode *left;
+    struct TreeLinkNode *right;
+    struct TreeLinkNode *next;
+    TreeLinkNode(int x) :val(x), left(NULL), right(NULL), next(NULL) {
+        
+    }
+};
+*/
+class Solution {
+public:
+    TreeLinkNode* GetNext(TreeLinkNode* pNode)
+    {
+        if(pNode == NULL)
+        {
+            return NULL;
+        }
+
+        TreeLinkNode *pNext = NULL;
+
+        //  如果当前结点有右子树, 那么其中序遍历的下一个结点就是其右子树的最左结点
+        if(pNode->right != NULL)
+        {
+            //  找到右子树的最左孩子
+            pNext = pNode->right;
+            while(pNext->left != NULL)
+            {
+                pNext = pNext->left;
+            }
+        }
+        else if(pNode->right == NULL && pNode->next != NULL)
+        {
+            TreeLinkNode *pCurrent = pNode;
+            TreeLinkNode *pParent = pNode->next;
+            //  如果当前结点是其父结点的左子结点那么其中序遍历的下一个结点就是他的父亲结点
+            //
+            //  如果当前结点是其父结点的右子结点，
+            //  这种情况下其下一个结点应该是当前结点所在的左子树的根
+            //  因此我们可以顺着其父节点一直向上遍历,
+            //  直到找到一个是它父结点的左子结点的结点
+            while(pParent != NULL && pCurrent == pParent->right)
+            {
+                pCurrent = pParent;
+                pParent = pParent->next;
+            }
+            pNext = pParent;
+        }
+
+        return pNext;
+    }
+};
