4d49e57b5415516d051c86195219a30ae272eb15
Yiif4n <45288677+Yiif4n@users.noreply.github.com>
2020-04-06 23:58:47 +0800

    Create 060-Print.py

---
+++ 060-把二叉树打印成多行/060-Print.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回二维列表[[1,2],[4,5]]
+    def Print(self, pRoot):
+        # write code here
+        if pRoot is None:
+            return []
+        ans=[]
+        queue=[]
+        queue.append(pRoot)
+        while queue:
+            temp=[]
+            for i in range(len(queue)):
+                node=queue.pop(0)
+                temp.append(node.val)
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+            ans.append(temp)
+        return ans
